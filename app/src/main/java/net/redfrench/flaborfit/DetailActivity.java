aa4ff5774ef8712b41ad143bdb0fa58d3546f65a
Red-French <red@redfrench.net>
2017-01-05 22:24:53 -0600

    initial commit

---
+++ app/src/main/java/net/redfrench/flaborfit/DetailActivity.java
@@ -0,0 +1,39 @@
+package net.redfrench.flaborfit;
+
+import android.graphics.Color;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);
+        ImageView exerciseImg = (ImageView)findViewById(R.id.exerciseImage);
+        LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBG);
+
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        exerciseText.setText(exerciseTitle);
+
+        if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS)) {
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.weight, getApplicationContext().getTheme()));  // getResources grabs the
+                                            // resources from your bundle where images, etc. are stored, but we want drawables (getDrawable()). Specifically,
+                                            // we want the weight image.
+                                            // The 2nd parameter is the theme you want to apply.
+                                                // getApplicationContext() gets the main context of the app.
+                                                // getTheme() is your main theme.
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+        } else if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)) {
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.lotus, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
+        } else
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.heart, getApplicationContext().getTheme()));
+            mainBG.setBackgroundColor(Color.parseColor("#52ad56"));
+    }
+}
