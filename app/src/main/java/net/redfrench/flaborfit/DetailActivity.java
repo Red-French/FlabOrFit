39bf6da53f92fa7c6a2bfa52cdf73b6c51d75fb4
Red-French <red@redfrench.net>
2017-01-22 23:43:06 -0600

    add comments

--- app/src/main/java/net/redfrench/flaborfit/DetailActivity.java
+++ app/src/main/java/net/redfrench/flaborfit/DetailActivity.java
@@ -14,21 +14,23 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);
+        // DOM references
+        TextView exerciseText = (TextView)findViewById(R.id.exerciseTitle);  // heading
         ImageView exerciseImg = (ImageView)findViewById(R.id.exerciseImage);
         LinearLayout mainBG = (LinearLayout)findViewById(R.id.mainBG);
 
-        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);
+        // update heading
+        String exerciseTitle = getIntent().getStringExtra(MainActivity.EXTRA_ITEM_TITLE);  // retrieve value of key (EXTRA_ITEM_TITLE) from MainActivity
         exerciseText.setText(exerciseTitle);
 
+        // update image and background color
         if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_WEIGHTS)) {
-            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.weight, getApplicationContext().getTheme()));  // getResources grabs the
-                                            // resources from your bundle where images, etc. are stored, but we want drawables (getDrawable()). Specifically,
-                                            // we want the weight image.
-                                            // The 2nd parameter is the theme you want to apply.
-                                                // getApplicationContext() gets the main context of the app.
-                                                // getTheme() is your main theme.
-            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));
+            exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.weight, getApplicationContext().getTheme()));
+                                      // getResources() - grabs the resources from your bundle where images, etc. are stored.
+                                      // getDrawable(R.drawable.weight) - Gets the weight image from the drawable resources
+                                      // The 2nd parameter (REQUIRED) of getDrawable() is the theme you want to apply.
+                                      // getApplicationContext() gets the main context of the app, and getTheme() is your main theme.
+            mainBG.setBackgroundColor(Color.parseColor("#2ca5f5"));  // parseColor turns the hex into a color
         } else if (exerciseTitle.equalsIgnoreCase(MainActivity.EXERCISE_YOGA)) {
             exerciseImg.setImageDrawable(getResources().getDrawable(R.drawable.lotus, getApplicationContext().getTheme()));
             mainBG.setBackgroundColor(Color.parseColor("#916bcd"));
